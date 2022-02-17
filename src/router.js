958204c0f2a1c9ea36213873e59345e8700956c3
Miriam T.G. Aarag <miriam.aarag@no.experis.com>
2022-02-17 15:44:44 +0100

    Initial setup.

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from "vue-router"
+import StartScreen from "./views/StartScreen.vue"
+
+const routes = [{
+    path: "/",
+    component: StartScreen
+}]
+
+export default createRouter({
+    history: createWebHistory(),
+    routes
+})
---
+++ src/views/StartScreen.vue
@@ -0,0 +1,3 @@
+<template>
+ <h1>Start screen</h1>
+</template>
